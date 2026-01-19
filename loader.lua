e9a95dbc0bbb06f95bc822ef7a8d8430f25f8c91
NNNNxfig <nennnn384@gmail.com>
2026-01-19 23:24:49 +0200

    в

--- loader.lua
+++ loader.lua
@@ -1,13 +1,10 @@
-local baseURL = "https://raw.githubusercontent.com/NNNNxfig/ggh/main/"
+_G.eNigma = _G.eNigma or {}
+_G.eNigma.baseURL = "https://raw.githubusercontent.com/NNNNxfig/ggh/main/"
 
 local function import(relPath)
-    local url = baseURL .. relPath
+    local url = _G.eNigma.baseURL .. relPath
     local src = game:HttpGet(url)
     return loadstring(src)()
 end
 
-local gui = import("Gui/defGui/gui.lua")
-
-if type(gui) == "table" and type(gui.init) == "function" then
-    gui.init()
-end
+import("Gui/defGui/gui.lua")
